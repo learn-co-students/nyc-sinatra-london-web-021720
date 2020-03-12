9b94d9694f753702be969eb233ba6117054475ed
Amaterasus <skyetay03@googlemail.com>
2020-03-12 16:29:33 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,48 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/new"
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :"figures/index"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @landmarks = @figure.landmarks
+    erb :"figures/show"
+  end
+
+  get '/figures/:id/edit' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    @figure = Figure.find(params[:id])
+    erb :"figures/edit"
+  end
+
+  patch '/figures/:id' do 
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    @landmark = Landmark.find_or_create_by(params[:landmark])
+    @landmark.update(figure: @figure)
+    redirect "/figures/#{@figure.id}"
+  end
+
+  post '/figures' do
+    figure = Figure.create(params["figure"])
+    if params["title"]["name"] != ""
+      title = Title.create(params["title"])
+      figure.titles << title
+    end
+    if params["landmark"]["name"] != ""
+      landmark = Landmark.create(params["landmark"])
+      figure.landmarks << landmark
+    end
+    figure.save
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,34 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"landmarks/index"
+  end
+  
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by(id: params[:id])
+    erb :"landmarks/edit"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by(id: params[:id])
+    erb :"landmarks/show"
+  end
+
+  post '/landmarks' do
+    Landmark.create params[:landmark]
+    redirect "/landmarks"
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by(params[:id])
+    @landmark.update(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
 end
